33121db12adc0e4d7272f8e55cbd77e95e29d176
Gunavanthvarma <gunavanthvarma.dev@gmail.com>
2026-05-23 22:01:33 -0400

    Refactor RaftNode struct fields to export and add NewRaftNode constructor

--- raftNode.go
+++ raftNode.go
@@ -17,21 +17,20 @@ const (
 )
 
 type RaftNode struct {
-	currentNodeId    NodeId
-	peers            []NodeId
-	electionTimeout  uint64 // random election timeout threshold
-	electionElapsed  uint64 //ticks since hearing from the leader
-	nodeStatus       RaftState
-	heartbeatElapsed uint64 //ticks since last haertbeat; for leader
-	heartbeatTimeout uint64 // frequency of heartbeats; for leader
-
+	CurrentNodeId    NodeId
+	Peers            []NodeId
+	ElectionTimeout  uint64 // random election timeout threshold
+	ElectionElapsed  uint64 //ticks since hearing from the leader
+	NodeStatus       RaftState
+	HeartbeatElapsed uint64 //ticks since last haertbeat; for leader
+	HeartbeatTimeout uint64 // frequency of heartbeats; for leader
 	//persistent state
 	currentTerm uint64 // latest term server has seen(default 0 at boot)
 	votedFor    NodeId //candidateID that received vote in current term(default 0 if none)
 	log         []LogEntry
 	//volatile state
-	commitIndex uint64 //index of highest log entry known to be committed
-	lastApplied uint64 //index of highest log entry applied to state machine
+	commitIndex uint64 //index of highest log entry known to be committed(default 0)
+	lastApplied uint64 //index of highest log entry applied to state machine(default 0)
 
 	//volatile state on all leaders; Reinitialized after each election
 	nextIndex map[NodeId]uint64 //for each server, index of the next log entry to send to that server(initizzalised to leader last log index+1 )
@@ -39,4 +38,36 @@ type RaftNode struct {
 	matchIndex map[NodeId]uint64 //for each server, index of the highest log entry known to be replicated on server
 }
 
-//
+//Methods:
+
+// 1. Create new RaftNode with config arguments NewRaftNode()
+// 2. ProcessNetworkMessage ---> procesess the message received from the server based on the message type:-
+//This will be a continuous running loop in the server that starts after Node creation
+//should i put the ticker update here?
+// 3. processAppendEntriesRequest()
+// 4. processAppendEntriesResponse()
+// 5. processRequestVoteRequest()
+// 6. processRequestVoteResponse()
+// 7. Tick()  ---> update time
+// 8. ProcessClientRequest() ---> process client requests
+// 9. ServerTasks() --- tells the server what it needs to do
+// 10. Proceed() --- tells Raft that everything was done
+
+func NewRaftNode(nodeid NodeId, peers []NodeId, electionTimeout uint64, heartbeatTimeout uint64) *RaftNode {
+
+	return &RaftNode{
+		CurrentNodeId:    nodeid,
+		Peers:            peers,
+		ElectionTimeout:  electionTimeout,
+		ElectionElapsed:  0,
+		NodeStatus:       Follower,
+		HeartbeatElapsed: 0,
+		HeartbeatTimeout: heartbeatTimeout,
+		currentTerm:      0,
+		votedFor:         0,
+		commitIndex:      0,
+		lastApplied:      0,
+		nextIndex:        make(map[NodeId]uint64),
+		matchIndex:       make(map[NodeId]uint64),
+	}
+}
