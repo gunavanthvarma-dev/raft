1c92f68a2dfe7b468eba7ac6d35ef3f4dd5e03de
Gunavanthvarma <gunavanthvarma.dev@gmail.com>
2026-06-13 17:21:00 -0400

    Enhance RaftNode structure with logIndex and leaderId; implement sendAppendEntries method for leader to send log entries to peers

--- raftNode.go
+++ raftNode.go
@@ -31,15 +31,17 @@ type RaftNode struct {
 	votedFor    NodeId //candidateID that received vote in current term(default 0 if none)
 	log         []LogEntry
 	//volatile state
+	logIndex    uint64 //next Index to insert in the log
 	commitIndex uint64 //index of highest log entry known to be committed(default 0)
 	lastApplied uint64 //index of highest log entry applied to state machine(default 0)
-
+	leaderId    NodeId //leaderId
 	//volatile state on all leaders; Reinitialized after each election
 	nextIndex map[NodeId]uint64 //for each server, index of the next log entry to send to that server(initizzalised to leader last log index+1 )
 
 	matchIndex  map[NodeId]uint64 //for each server, index of the highest log entry known to be replicated on server
 	timeoutGen  *TimeoutGenerator //generate timeout
 	serverTasks *ServerTasks
+	majority    uint64 //tracks the number of AppendEntriesResponse received to advance commitIndex
 }
 
 //Methods:
@@ -70,6 +72,7 @@ func NewRaftNode(nodeid NodeId, peers []NodeId, electionTimeout uint64, heartbea
 		ElectionVotes:    0,
 		currentTerm:      0,
 		votedFor:         0,
+		logIndex:         0,
 		commitIndex:      0,
 		lastApplied:      0,
 		nextIndex:        make(map[NodeId]uint64),
@@ -123,15 +126,72 @@ func (node *RaftNode) sendRequestVote() {
 	}
 }
 
+func (node *RaftNode) sendAppendEntries() {
+	if node.NodeStatus != Leader {
+		//log error
+		return
+	}
+	for _, peerId := range node.Peers {
+		var msg *Message
+		if node.log[len(node.log)-1].Index >= node.nextIndex[peerId] {
+
+			msg = &Message{Type: AppendEntriesRequest,
+				FromNodeId:   node.CurrentNodeId,
+				ToNodeId:     peerId,
+				Term:         node.currentTerm,
+				LeaderId:     node.CurrentNodeId,
+				PrevLogIndex: node.log[node.nextIndex[peerId]-1].Index,
+				PrevLogTerm:  node.log[node.nextIndex[peerId]-1].Term,
+				Entries:      node.log[node.nextIndex[peerId]:],
+				LeaderCommit: node.commitIndex,
+			}
+		} else {
+			msg = &Message{Type: AppendEntriesRequest,
+				FromNodeId:   node.CurrentNodeId,
+				ToNodeId:     peerId,
+				Term:         node.currentTerm,
+				LeaderId:     node.CurrentNodeId,
+				PrevLogIndex: 0,
+				PrevLogTerm:  0,
+				Entries:      make([]LogEntry, 0),
+				LeaderCommit: node.commitIndex,
+			}
+			//send Heartbeat
+		}
+		node.serverTasks.Messages = append(node.serverTasks.Messages, *msg) //append messages to serverTasks
+	}
+
+}
+
 func (node *RaftNode) ProcessClientRequest(req ClientRequest) {
 	//if current node is follower, reject request and send the latest leader info (need to decide on how to do it;add to ServerTasks)
+	//create a ClientRedirect message, give leader Id and success as false
 	//if current node is candidate, reject request(send an error response and tell to try again;add to ServerTasks)
+	//create a ClientError message, and put success as false
 	//if current node is leader, process the request
 	//create new LogEntry
 	//append logentry to leader's log
+	//persist; add to ServerTasks
 	//Build AppendEntriesRPC for all peers
 	//send it to all peers(through serverTasks)
 	//so the leader has to wait for majority replication before applyting the entry to the state machine(do we need a state variable to track whether the leader is waiting for majority replication and also track the majority count)
+
+	switch node.NodeStatus {
+	case Follower:
+		msg := &Message{Type: ClientRedirect, FromNodeId: node.CurrentNodeId, LeaderId: node.leaderId, Success: false}
+		node.serverTasks.Messages = append(node.serverTasks.Messages, *msg)
+
+	case Candidate:
+		msg := &Message{Type: ClientError, FromNodeId: node.CurrentNodeId, Success: false}
+		node.serverTasks.Messages = append(node.serverTasks.Messages, *msg)
+
+	case Leader:
+		node.logIndex += 1
+		logEntry := &LogEntry{Index: node.logIndex, Term: node.currentTerm, Command: req.Data}
+		node.log = append(node.log, *logEntry)
+		node.serverTasks.EntriesToPersist = append(node.serverTasks.EntriesToPersist, *logEntry)
+		node.sendAppendEntries() //send AppendEntriesRPC to all followers
+	}
 
 }
 
@@ -150,6 +210,7 @@ func (node *RaftNode) ProcessNetworkMessage(msg Message) {
 	//	1.2.2 if log[msg.prevLogIndex] not exist || log[msg.prevLogIndex].term!=prevLogTerm
 	//	same as above
 	//
+	//		node.leaderId = msg.FromId
 	//		check = Entries[0]
 	//		if check.term!= log[check.index].term
 	//			remove everything from current entry to end of log
@@ -163,7 +224,8 @@ func (node *RaftNode) ProcessNetworkMessage(msg Message) {
 	//			1.3.1.2 term = currentTerm
 	//			1.3.1.3 create AppendEntriesResponse and add to serverTasks
 	//		1.3.2 else
-	//			1.3.2.1 change nodeStatus to Follower; and process the message as a follower   //redundant
+	//			1.3.2.1 node.leaderId = msg.FromId
+	//			1.3.2.2 change nodeStatus to Follower; and process the message as a follower   //redundant
 	//1.4 Leader
 	//1.4.1 if msg.term < currentTerm
 	//		same as above
